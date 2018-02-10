4e249ab2bff3d8dfdab9fe31082783f8e11664cd
Rahul Kadam <rkadam@corp.qualys.com>
2018-02-10 22:20:32 +0530

    adding hacker rank share market problem

---
+++ src/main/java/com/LeetCode.java
@@ -0,0 +1,362 @@
+package com;
+
+import com.rx.dto.Result;
+
+import java.util.*;
+import java.util.stream.Collectors;
+
+public class LeetCode {
+
+    public static void main(String args[]) {
+        LeetCode leetCode = new LeetCode();
+        String[] query = {"N,1,0000001,AB,L,B,104.53,100" ,
+                "N,2,0000001,AB,L,B,104.53,100" ,
+                "N,3,0000001,AB,L,B,104.53,100" ,
+                "N,12,0000001,XYZ,L,B,103.53,150" ,
+                "A,2,0000001,AB,L,B,104.53,150" ,
+                "N,4,0000001,XYZ,L,S,103.53,150" ,
+                "N,5,0000001,AB,L,S,104.53,80" ,
+                "N,6,0000001,AB,L,S,104.53,20" ,
+                "X,1,0000001",
+                "X,11,0000001",
+                "Q"};
+        String q[] = {
+                "N,1,0000001,ALN,L,B,60.90,100",
+                "N,13,0000002,ALN,L,B,60.90,100",
+                "N,10,0000003,ALN,L,S,60.90,100",
+                "N,12,0000004,ALN,L,S,60.90,100",
+                "N,11,0000005,ALB,L,S,60.90,100",
+                "N,14,0000006,ALB,L,S,62.90,101",
+                "N,16,0000007,ALB,L,S,63.90,102",
+                "N,18,0000008,ALB,L,S,64.90,103",
+                "N,20,0000009,ALB,L,S,65.90,104",
+                "Q,0000003" ,
+                "Q,ALB",
+                "Q,ALN,0000002",
+                "Q,0000002,ALN",
+                "Q"
+        };
+        String [] sampleInput= {
+            "N,1,0000001,AB,L,B,104.53,100",
+            "N,2,0000002,AB,L,S,105.53,100",
+            "N,3,0000003,AB,L,B,104.53,90",
+  //          "M,0000004",
+            "N,4,0000005,AB,L,S,104.43,80" ,
+            "A,2,0000006,AB,L,S,104.42,100",
+            "Q",
+//            "M,0000008",
+            "N,5,0000009,AB,L,S,105.53,120",
+            "X,3,0000010",
+            "N,6,0000011,XYZ,L,B,1214.82,2568",
+            "Q"
+        };
+
+        String matchQuery[] = {
+                "N,1,0000001,ALN,L,B,60.90,100",
+                "N,11,0000002,XYZ,L,B,60.90,200",
+                "N,110,0000003,XYZ,L,S,60.90,100",
+                "N,112,0000003,XYZ,L,S,60.90,120",
+                "N,10,0000006,ALN,L,S,60.90,100",
+                "M,00010",
+                "M,00010,ALN"
+        };
+        String result[] = processQueries(matchQuery);
+        for(int i=0;i < result.length;i++) {
+            System.out.println(result[i]);
+        }
+    }
+
+    static String[] processQueries(String[] queries) {
+        // Write your code here.
+        Map<Long,HashMap<String, Object>> orderDetails = new HashMap<>();
+        Map<String,HashMap<Long, HashMap<String, Object>>> buyOrders = new HashMap<>();
+        Map<String,HashMap<Long, HashMap<String, Object>>> sellOrders = new HashMap<>();
+        List<HashMap<String, Object>> orderList = new ArrayList<>();
+
+        List<String> resultList = new ArrayList<>();
+        for(String query : queries) {
+            String actionType = getActionType(query);
+            switch(actionType) {
+                case "N":
+                    try {
+                        HashMap<String, Object> orderMap = getOrderMap(query);
+                        boolean validate = validateNewOrder(orderDetails, orderMap);
+                        if (validate) {
+                            orderDetails.put((Long)orderMap.get("OrderId") , orderMap);
+                            orderList.add(orderMap);
+                            resultList.add((Long)orderMap.get("OrderId") + " - Accept");
+                        } else {
+                            resultList.add((Long)orderMap.get("OrderId") + " - Reject - 303 - Invalid order details");
+                        }
+                    } catch (Exception e) {
+                        resultList.add(" - Reject - 303 - Invalid order details");
+                    }
+                    break;
+                case "A" :
+                    try {
+                        HashMap<String, Object> orderMap = getOrderMap(query);
+                        boolean validateExistance = validateNewOrder(orderDetails, orderMap);
+                        if (validateExistance) {
+                            resultList.add((Long)orderMap.get("OrderId") + " - AmendReject - 404 - Order does not exist");
+                        } else {
+                            boolean amendValidate = validateAmendOrder(orderDetails, orderMap);
+                            if (amendValidate) {
+                                orderDetails.put((Long)orderMap.get("OrderId") , orderMap);
+                                resultList.add((Long)orderMap.get("OrderId") + " - AmendAccept");
+                            } else {
+                              resultList.add((Long)orderMap.get("OrderId") + " - AmendReject - 101 - Invalid amendment details");
+                            }
+                        }
+                    } catch (Exception e) {
+                            e.printStackTrace();
+                    }
+                    break;
+                case "X" :
+                        try{
+                            Long orderId = getOrderId(query);
+                            boolean validateCancel = validateCancelOrder(orderId , orderDetails);
+                            if (validateCancel) {
+                                orderDetails.remove(orderId);
+                                resultList.add(orderId + " - CancelAccept");
+                            } else {
+                                resultList.add(orderId + " - CancelReject - 404 - Order does not exist");
+                            }
+                        } catch(Exception e) {
+
+                        }
+                    break;
+                case "Q":
+                    List<HashMap<String, Object>> queryResult = fetchQueryResult(query , orderList);
+                    manageQueryResult(queryResult,resultList);
+                    break;
+                case "M":
+                    List<HashMap<String, Object>> matchResult = fetchQueryResult(query , orderList);
+                    manageMatchResult(matchResult,resultList , orderList);
+                    break;
+                default:
+                    break;
+            }
+        }
+        String [] resultString =(String[])resultList.toArray(new String[resultList.size()]);
+        return resultString;
+    }
+
+    static boolean validateNewOrder(Map<Long,HashMap<String, Object>> orderDetails , HashMap<String, Object> orderMap) {
+
+        try {
+            if (!orderDetails.containsKey((Long)orderMap.get("OrderId"))) {
+                return true;
+            } else {
+                return false;
+            }
+        } catch (Exception e) {
+            return false;
+        }
+    }
+
+    static void manageQueryResult(List<HashMap<String, Object>> orderList , List<String> resultList) {
+        Set<String> uniqueProductList = orderList.stream().map(a -> (String)a.get("Symbol")).collect(Collectors.toCollection(TreeSet::new));
+
+        uniqueProductList.forEach(order -> {
+            List<HashMap<String, Object>> buyerList = orderList.stream().filter(a -> {
+                return ((String) a.get("Side")).equals("B") && a.get("Symbol").equals(order);
+            }).sorted((a, b) -> {
+                return ((Float) a.get("Price")).compareTo((Float) b.get("Price"));
+            }).collect(Collectors.toList());
+
+            List<HashMap<String, Object>> sellerList = orderList.stream().filter(a -> {
+                return ((String) a.get("Side")).equals("S") && a.get("Symbol").equals(order);
+            }).sorted((a, b) -> {
+                return ((Float) a.get("Price")).compareTo((Float) b.get("Price"));
+            }).collect(Collectors.toList());
+
+            Iterator<HashMap<String, Object>> buyIterator = buyerList.listIterator();
+            int i=0;
+            while(buyIterator.hasNext() && i < sellerList.size()) {
+                HashMap<String, Object> buyerBid = buyIterator.next();
+                HashMap<String, Object> sellerBid = sellerList.get(i);
+                if ((Float)sellerBid.get("Price") <= (Float)buyerBid.get("Price") && (Long)sellerBid.get("Quantity") <= (Long)buyerBid.get("Quantity")){
+                    updateResultList(resultList , buyerBid , sellerBid , order);
+                    i++;
+                } else {
+                    updateResultList(resultList , buyerBid , null , order);
+                }
+            }
+            while(buyIterator.hasNext()) {
+                HashMap<String, Object> buyerBid = buyIterator.next();
+                updateResultList(resultList , buyerBid , null , order);
+            }
+            while(i < sellerList.size()) {
+                updateResultList(resultList , null , sellerList.get(i) , order);
+                i++;
+            }
+        });
+    }
+
+    static void manageMatchResult(List<HashMap<String, Object>> orderList , List<String> resultList , List<HashMap<String, Object>> orderUpdatedList) {
+        Set<String> uniqueProductList = orderList.stream().map(a -> (String)a.get("Symbol")).collect(Collectors.toCollection(TreeSet::new));
+
+        uniqueProductList.forEach(order -> {
+            List<HashMap<String, Object>> buyerList = orderList.stream().filter(a -> {
+                return ((String) a.get("Side")).equals("B") && a.get("Symbol").equals(order);
+            }).sorted((a, b) -> {
+                return ((Float) a.get("Price")).compareTo((Float) b.get("Price"));
+            }).collect(Collectors.toList());
+
+            List<HashMap<String, Object>> sellerList = orderList.stream().filter(a -> {
+                return ((String) a.get("Side")).equals("S") && a.get("Symbol").equals(order);
+            }).sorted((a, b) -> {
+                return ((Float) b.get("Price")).compareTo((Float) a.get("Price"));
+            }).collect(Collectors.toList());
+
+            Iterator<HashMap<String, Object>> buyIterator = buyerList.listIterator();
+            int sellerIndex = 0;
+            int buyerIndex = 0;
+            while( buyerIndex < buyerList.size() && sellerIndex < sellerList.size()) {
+                HashMap<String, Object> buyerBid = buyerList.get(buyerIndex);
+                HashMap<String, Object> sellerBid = sellerList.get(sellerIndex);
+                if ((Float)sellerBid.get("Price") <= (Float)buyerBid.get("Price")){
+                    if ((Long)sellerBid.get("Quantity") >= (Long)buyerBid.get("Quantity")) {
+                        sellerBid.put("Quantity", (Long) sellerBid.get("Quantity") - (Long) buyerBid.get("Quantity"));
+                        buyerIndex++;
+                    } else {
+                        buyerBid.put("Quantity", (Long) buyerBid.get("Quantity") - (Long) sellerBid.get("Quantity"));
+                        sellerIndex++;
+                    }
+                    updateResultList(resultList , buyerBid , sellerBid , order);
+                } else {
+                    buyerIndex++;
+                }
+            }
+        });
+    }
+
+    static void updateResultList(List<String> resultList , HashMap<String, Object> buyerBid , HashMap<String, Object> sellerBid , String symbol) {
+        String result = symbol +"|";
+        if (buyerBid != null) {
+            result += buyerBid.get("OrderId") +","+buyerBid.get("OrderType") +"," + buyerBid.get("Quantity") + "," + buyerBid.get("Price") +"|";
+        } else {
+            result += "|";
+        }
+        if (sellerBid != null) {
+            result += sellerBid.get("Price") +","+sellerBid.get("Quantity") +"," + sellerBid.get("OrderType") + "," + sellerBid.get("OrderId");
+        }
+        resultList.add(result);
+    }
+
+
+    static List<HashMap<String, Object>> fetchQueryResult(String order , List<HashMap<String, Object>> orderList) {
+        String orderDetails[] = order.split(",");
+        List<HashMap<String, Object>> resultlist = null;
+        if (orderDetails.length ==1) {
+             resultlist = orderList.stream().sorted((a, b)-> {
+                 return ((String)a.get("Symbol")).compareTo((String)b.get("Symbol"));
+             }).collect(Collectors.toList());
+        } else if(orderDetails.length ==2) {
+            String str = orderDetails[1];
+            try {
+                Long timeStamp = Long.parseLong(str);
+                resultlist = orderList.stream().filter(obj -> {
+                    return (Long)obj.get("Timestamp") <= timeStamp;
+                }).collect(Collectors.toList());
+            } catch(Exception e){
+                 resultlist = orderList.stream().filter(obj -> {
+                    return obj.get("Symbol").equals(str);
+                }).collect(Collectors.toList());
+            }
+        } else if(orderDetails.length ==3) {
+            String str = orderDetails[1];
+            String secondArg = orderDetails[2];
+            try {
+                Long timeStamp = Long.parseLong(str);
+                resultlist = orderList.stream().filter(obj -> {
+                    return (Long)obj.get("Timestamp") <= timeStamp && secondArg.equals(obj.get("Symbol"));
+                }).collect(Collectors.toList());
+            } catch(Exception e){
+                resultlist = orderList.stream().filter(obj -> {
+                    return obj.get("Symbol").equals(str) && (Long)obj.get("Timestamp") <= Long.parseLong(secondArg);
+                }).collect(Collectors.toList());
+            }
+        }
+        return resultlist;
+    }
+
+    static boolean validateCancelOrder(Long orderId , Map<Long,HashMap<String, Object>> orderDetails) {
+
+        try {
+            if (orderDetails.containsKey(orderId)) {
+                return true;
+            } else {
+                return false;
+            }
+        } catch (Exception e) {
+            return false;
+        }
+    }
+
+    static boolean validateAmendOrder(Map<Long,HashMap<String, Object>> orderDetails , HashMap<String, Object> orderMap) {
+        HashMap<String, Object> existingOrder = orderDetails.get((Long)orderMap.get("OrderId"));
+        if (existingOrder.get("Side").equals(orderMap.get("Side")) &&
+                existingOrder.get("OrderType").equals(orderMap.get("OrderType")) && existingOrder.get("Symbol").equals(orderMap.get("Symbol"))) {
+            return true;
+        }
+        return false;
+    }
+
+    static HashMap<String, Object> getOrderMap(String order)  throws Exception{
+        HashMap<String, Object> orderMap = new HashMap<>();
+        String orderDetails[] = order.split(",");
+        int i=1;
+        Long id = 0L;
+        for(String str : orderDetails) {
+            switch (i) {
+                case 1:
+                    orderMap.put("Action" , str);
+                    break;
+                case 2:
+                    id = Long.parseLong(str);
+                    orderMap.put("OrderId" , id);
+                    break;
+                case 3:
+                    id = Long.parseLong(str);
+                    orderMap.put("Timestamp" , id);
+                    break;
+                case 4:
+                    orderMap.put("Symbol" , str);
+                    break;
+                case 5:
+                    orderMap.put("OrderType" , str);
+                    break;
+                case 6:
+                    orderMap.put("Side" , str);
+                    break;
+                case 7:
+                    Float price = Float.parseFloat(str);
+                    orderMap.put("Price" , price);
+                    break;
+                case 8:
+                    id = Long.parseLong(str);
+                    orderMap.put("Quantity" , id);
+                    break;
+                default :
+                    break;
+            }
+            i++;
+        }
+
+        if (i!=9) {
+            throw new Exception("invalid request");
+        }
+        return orderMap;
+    }
+
+    static String getActionType(String order) {
+        String orderDetails[] = order.split(",");
+        return orderDetails[0];
+    }
+
+    static Long getOrderId(String order) {
+        String orderDetails[] = order.split(",");
+        return Long.parseLong(orderDetails[1]);
+    }
+}
--- src/main/java/com/rx/sample.java
+++ src/main/java/com/rx/sample.java
@@ -25,11 +25,176 @@ public class sample {
     public DataStore dataStore;
     public static void main(String[] args) {
         Flowable.just("Hello world").subscribe((a) -> {
-            System.out.println(a);
+         //   System.out.println(a);
         });
         sample s= new sample();
-        s.initialDataStore();
-        s.PrintObserverExample();
+       // s.initialDataStore();
+       // s.PrintObserverExample();
+        int arr[] = {-1,1,-1,1};
+        int originalArray[] = {1,5,2,-1};
+        //int B[] = s.solutionB(5,arr);
+        for (int i=0; i< arr.length ; i++) {
+//            s.Combinations(arr,arr.length,i);
+          //  System.out.print("  " + B[i]);
+        }
+        System.out.println(s.permuteHelper(arr,0 , originalArray , 0));
+    }
+
+    private int permuteHelper(int[] arr, int index ,int[] originalArray , int osum){
+        if(index >= arr.length - 1){
+
+            System.out.print("[");
+            for(int i = 0; i < arr.length - 1; i++){
+                System.out.print(arr[i] + ", ");
+            }
+            if(arr.length > 0)
+                System.out.print(arr[arr.length - 1]);
+            System.out.println("]");
+
+            int sum = 0;
+            for(int i=0;i< arr.length; i++) {
+                sum = sum + arr[i]*originalArray[i];
+            }
+            if ( osum <= sum) {
+                return osum;
+            }
+            return sum;
+        }
+
+        int sum1 = osum;
+        for(int i = index; i < arr.length; i++){ //For each index in the sub array arr[index...end]
+
+            //Swap the elements at indices index and i
+            int t = arr[index];
+            arr[index] = arr[i];
+            arr[i] = t;
+
+            //Recurse on the sub array arr[index+1...end]
+            sum1 = permuteHelper(arr, index+1 , originalArray , sum1);
+            System.out.println("Answer " +sum1);
+            //Swap the elements back
+            t = arr[index];
+            arr[index] = arr[i];
+            arr[i] = t;
+        }
+        return sum1;
+    }
+
+    public int Combinations(int[] A , int length , int current) {
+        if(length-1 == current) {
+            System.out.println(A[current]);
+        } else {
+            System.out.print(A[current]);
+            Combinations(A, length, current + 1);
+            if(current+2 < length) {
+                Combinations(A, length, current + 2);
+            }
+        }
+        return 0;
+    }
+
+    public int DP(int[] A) {
+        // write your code in Java SE 8
+        int length = A.length;
+        int S[] = new int[length];
+        for(int i=0;i<length ; i++) {
+            S[i] = i%2==0 ? 1: -1;
+        }
+        int sum =0;
+        for(int i=0;i< length; i++) {
+            sum = sum + A[i]*S[i];
+        }
+        return sum;
+    }
+
+    // you can also use imports, for example:
+// import java.util.*;
+
+// you can write to stdout for debugging purposes, e.g.
+// System.out.println("this is a debug message");
+
+    public int solutionA(int[] A) {
+        int length = A.length;
+        int B[] = new int[length];
+        for (int i = 0; i < length; i++) {
+            if (A[i] > 0 && A[i] < length) {
+                B[A[i]] = A[i];
+            }
+        }
+        if (length == 1) {
+            if (A[0] == 1) {
+                return 2;
+            }
+            return 1;
+        }
+        for (int i = 1; i < length; i++) {
+            if (B[i] == 0) {
+                return i;
+            }
+        }
+        return length + 1;
+    }
+
+    public int solution(int[] A) {
+        int length = A.length;
+        if (length == 1) {
+            if (A[0] == 1) {
+                return 2;
+            }
+            return 1;
+        }
+        for (int i = 0; i < length; i++) {
+            int currentElement = A[i];
+            while(currentElement > 0 && currentElement < length && A[currentElement] != currentElement) {
+                int abc = A[currentElement];
+                A[currentElement] = currentElement;
+                currentElement = abc;
+            }
+        }
+        for (int i = 0; i < length; i++) {
+            if (A[i] != i) {
+                return i;
+            }
+        }
+        return length + 1;
+    }
+
+    /*
+    A[0] = 3
+    A[1] = 4
+    A[2] = 4
+    A[3] = 6
+    A[4] = 1
+    A[5] = 4
+    A[6] = 4
+the values of the counters after each consecutive operation will be:
+
+    (0, 0, 1, 0, 0)
+    (0, 0, 1, 1, 0)
+    (0, 0, 1, 2, 0)
+    (2, 2, 2, 2, 2)
+    (3, 2, 2, 2, 2)
+    (3, 2, 2, 3, 2)
+    (3, 2, 2, 4, 2)
+     */
+    public int[] solutionB(int N, int[] A) {
+        // write your code in Java SE 8
+
+        int B[] = new int[N];
+        int maxCounter = 0;
+        for ( int i =0; i < A.length ; i++) {
+            if (A[i] == N+1) {
+                for(int j = 0; j< N; j++) {
+                    B[j] = maxCounter;
+                }
+            } else {
+                ++B[A[i]-1];
+                if (maxCounter < B[A[i]-1]) {
+                    maxCounter = B[A[i]-1];
+                }
+            }
+        }
+        return B;
     }
 
     public void initialDataStore() {
@@ -42,6 +207,7 @@ public class sample {
     public void printCollegedata(List<College> list) {
         System.out.println("College : "+ list);
     }
+
 
 
     /**
@@ -97,7 +263,11 @@ public class sample {
           System.out.println("starting : ");
           Flowable.just(new DataStore().getStudentData()).subscribe(getSubscriber());
           System.out.println("after Student: ");
-          Flowable.just(new DataStore().getCollegeData()).subscribeOn(Schedulers.io()).observeOn(Schedulers.single()).subscribe(a -> printCollegedata(a));
+
+          Flowable.just(new DataStore().getCollegeData()).
+                  subscribeOn(Schedulers.io()).observeOn(Schedulers.single()).subscribe(a -> printCollegedata(a));
+
+
           System.out.println("after College: ");
           new DataStore().getResultData().subscribe(System.out::println);
 
